11be3886e92ecbc5c95495a5150a112e2be0fdcb
Dustin Diaz <polvero@gmail.com>
2011-09-15 10:53:26 -0700

    - le style

--- ender.js
+++ ender.js
@@ -1,7 +1,7 @@
 /*!
-  * Ender-JS: open module JavaScript framework (client-lib)
+  * Ender: open module JavaScript framework (client-lib)
   * copyright Dustin Diaz & Jacob Thornton 2011 (@ded @fat)
-  * https://ender.no.de
+  * http://ender.no.de
   * License MIT
   */
 !function (context) {
@@ -9,81 +9,75 @@
   // a global object for node.js module compatiblity
   // ============================================
 
-  context['global'] = context;
+  context['global'] = context
 
   // Implements simple module system
   // losely based on CommonJS Modules spec v1.1.1
   // ============================================
 
-  var modules = {};
+  var modules = {}
+    , old = context.$
 
   function require (identifier) {
-    var module = modules[identifier] || window[identifier];
-    if (!module) throw new Error("Requested module '" + identifier + "' has not been defined.");
-    return module;
+    var module = modules[identifier] || window[identifier]
+    if (!module) throw new Error("Requested module '" + identifier + "' has not been defined.")
+    return module
   }
 
   function provide (name, what) {
-    return modules[name] = what;
+    return modules[name] = what
   }
 
-  context['provide'] = provide;
-  context['require'] = require;
+  context['provide'] = provide
+  context['require'] = require
 
   // Implements Ender's $ global access object
   // =========================================
 
   function aug(o, o2) {
-    for (var k in o2) {
-      k != 'noConflict' && k != '_VERSION' && (o[k] = o2[k]);
-    }
-    return o;
+    for (var k in o2) k != 'noConflict' && k != '_VERSION' && (o[k] = o2[k])
+    return o
   }
 
   function boosh(s, r, els) {
-                          // string || node || nodelist || window
+    // string || node || nodelist || window
     if (ender._select && (typeof s == 'string' || s.nodeName || s.length && 'item' in s || s == window)) {
-      els = ender._select(s, r);
-      els.selector = s;
-    } else {
-      els = isFinite(s.length) ? s : [s];
-    }
-    return aug(els, boosh);
+      els = ender._select(s, r)
+      els.selector = s
+    } else els = isFinite(s.length) ? s : [s]
+    return aug(els, boosh)
   }
 
   function ender(s, r) {
-    return boosh(s, r);
+    return boosh(s, r)
   }
 
   aug(ender, {
-    _VERSION: '0.2.5',
+    _VERSION: '0.3.2',
     ender: function (o, chain) {
-      aug(chain ? boosh : ender, o);
+      aug(chain ? boosh : ender, o)
     },
     fn: context.$ && context.$.fn || {} // for easy compat to jQuery plugins
-  });
+  })
 
   aug(boosh, {
     forEach: function (fn, scope, i) {
       // opt out of native forEach so we can intentionally call our own scope
       // defaulting to the current item and be able to return self
-      for (i = 0, l = this.length; i < l; ++i) {
-        i in this && fn.call(scope || this[i], this[i], i, this);
-      }
+      for (i = 0, l = this.length; i < l; ++i) i in this && fn.call(scope || this[i], this[i], i, this)
       // return self for chaining
-      return this;
+      return this
     },
     $: ender // handy reference to self
-  });
+  })
 
-  var old = context.$;
   ender.noConflict = function () {
-    context.$ = old;
-    return this;
-  };
+    context.$ = old
+    return this
+  }
 
-  (typeof module !== 'undefined') && module.exports && (module.exports = ender);
+  (typeof module !== 'undefined') && module.exports && (module.exports = ender)
   // use subscript notation as extern for Closure compilation
-  context['ender'] = context['$'] = context['ender'] || ender;
+  context['ender'] = context['$'] = context['ender'] || ender
 
 }(this);
